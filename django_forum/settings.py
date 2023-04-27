ebc25d416fe43427d667c562148a515bab6c5b51
Daria <dariaraby1@gmail.com>
2023-04-27 13:00:02 -0700

    updated

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 X_FRAME_OPTIONS = '*'
 CORS_ORIGIN_ALLOW_ALL = True
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'https://twitterclone.dariaraby.repl.co']
 
 # Application definition
 
